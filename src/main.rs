139c36e4d0eec8b6a115aa70782094b8e9a9a662
Gabriel Smith <ga29smith@gmail.com>
2017-08-31 22:58:02 -0400

    Use UART to print panics
    This now matches the functionality reached at the end of Part 2 of the tutorial.
    
    Signed-off-by: Gabriel Smith <ga29smith@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(lang_items,asm)]
+#![feature(lang_items,asm,drop_types_in_const)]
 #![no_std]
 #![no_main]
 #![no_builtins]
@@ -16,10 +16,13 @@ mod watchdog;
 use core::slice;
 use core::fmt::Write;
 
+use volatile::Volatile;
+
 use mcg::{Clock,Mcg,OscRange};
 use osc::Osc;
-use port::PortName;
+use port::{Port,PortName};
 use sim::Sim;
+use uart::Uart;
 use watchdog::Watchdog;
 
 extern {
@@ -42,6 +45,9 @@ pub static _FLASHCONFIG: [u8; 16] = [
     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
     0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF
 ];
+
+static mut PANIC_PORT: Option<Port> = None;
+static mut PANIC_WRITER: Option<Uart<'static, 'static>> = None;
 
 #[no_mangle]
 pub extern fn main() {
@@ -75,12 +81,13 @@ pub extern fn main() {
         panic!("Somehow the clock wasn't in FEI mode");
     }
 
-    let port_b = sim.port(PortName::B);
-    let rx = port_b.pin(16).to_uart_rx().unwrap();
-    let tx = port_b.pin(17).to_uart_tx().unwrap();
-    let mut uart =sim.uart(0, Some(rx), Some(tx), (468, 24)).unwrap();
-
-    writeln!(uart, "Hello World").unwrap();
+    unsafe {
+        PANIC_PORT = Some(sim.port(PortName::B));
+        let rx = PANIC_PORT.as_ref().unwrap().pin(16).to_uart_rx().unwrap();
+        let tx = PANIC_PORT.as_ref().unwrap().pin(17).to_uart_tx().unwrap();
+        PANIC_WRITER = Some(sim.uart(0, Some(rx), Some(tx), (468, 24)).unwrap());
+        writeln!(PANIC_WRITER.as_mut().unwrap(), "Hello World").unwrap();
+    }
 
     let port_c = sim.port(PortName::C);
     let mut gpio = port_c.pin(5).to_gpio();
@@ -92,10 +99,23 @@ pub extern fn main() {
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn rust_begin_unwind(_msg: core::fmt::Arguments,
-                                _file: &'static str,
-                                _line: u32) -> ! {
-    loop {};
+pub extern fn rust_begin_unwind(msg: core::fmt::Arguments,
+                                file: &'static str,
+                                line: u32) -> ! {
+    if let Some(uart) = unsafe { PANIC_WRITER.as_mut() } {
+        write!(uart, "panicked at '").unwrap();
+        uart.write_fmt(msg).unwrap();
+        write!(uart, "', {}:{}\n", file, line).unwrap();
+    }
+
+    // Reset the MCU after we've printed our panic.
+    /*
+    let aircr = unsafe {
+        &mut *(0xE000ED0C as *mut Volatile<u32>)
+    };
+    aircr.write(0x05FA0004);
+    */
+    loop {}
 }
 
 #[lang = "eh_personality"]
