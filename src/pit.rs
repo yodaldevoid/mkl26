26e9c67c3fee0b3f1c437639f00887d80d0d4896
Gabriel Smith <ga29smith@gmail.com>
2018-11-13 17:18:19 -0500

    pit: correct field names used in register adress test
    Signed-off-by: Gabriel Smith <ga29smith@gmail.com>

--- src/pit.rs
+++ src/pit.rs
@@ -131,17 +131,17 @@ mod tests {
     fn pit_test() {
         unsafe {
             let reg = & *(PIT_ADDR as *const PitRegs);
-            assert_eq!(0x4003_7000 as *const RW<u32>, &reg.mcr                  as *const RW<u32>, "mcr");
-            assert_eq!(0x4003_70E0 as *const RO<u32>, &reg.ltmr64h              as *const RO<u32>, "ltmr64h");
-            assert_eq!(0x4003_70E4 as *const RO<u32>, &reg.ltmr64l              as *const RO<u32>, "ltmr64l");
-            assert_eq!(0x4003_7100 as *const RW<u32>, &reg.timer[0].ldvalx    as *const RW<u32>, "ldval0");
-            assert_eq!(0x4003_7104 as *const RO<u32>, &reg.timer[0].cvalx     as *const RO<u32>, "cval0");
-            assert_eq!(0x4003_7108 as *const RW<u32>, &reg.timer[0].tctrlx    as *const RW<u32>, "tctrl0");
-            assert_eq!(0x4003_710C as *const RW<u32>, &reg.timer[0].tflgx     as *const RW<u32>, "tflg0");
-            assert_eq!(0x4003_7110 as *const RW<u32>, &reg.timer[1].ldvalx    as *const RW<u32>, "ldval1");
-            assert_eq!(0x4003_7114 as *const RO<u32>, &reg.timer[1].cvalx     as *const RO<u32>, "cval1");
-            assert_eq!(0x4003_7118 as *const RW<u32>, &reg.timer[1].tctrlx    as *const RW<u32>, "tctrl1");
-            assert_eq!(0x4003_711C as *const RW<u32>, &reg.timer[1].tflgx     as *const RW<u32>, "tflg1");
+            assert_eq!(0x4003_7000 as *const RW<u32>, &reg.mcr              as *const RW<u32>, "mcr");
+            assert_eq!(0x4003_70E0 as *const RO<u32>, &reg.ltmr64h          as *const RO<u32>, "ltmr64h");
+            assert_eq!(0x4003_70E4 as *const RO<u32>, &reg.ltmr64l          as *const RO<u32>, "ltmr64l");
+            assert_eq!(0x4003_7100 as *const RW<u32>, &reg.timer[0].ldvaln  as *const RW<u32>, "ldval0");
+            assert_eq!(0x4003_7104 as *const RO<u32>, &reg.timer[0].cvaln   as *const RO<u32>, "cval0");
+            assert_eq!(0x4003_7108 as *const RW<u32>, &reg.timer[0].tctrln  as *const RW<u32>, "tctrl0");
+            assert_eq!(0x4003_710C as *const RW<u32>, &reg.timer[0].tflgn   as *const RW<u32>, "tflg0");
+            assert_eq!(0x4003_7110 as *const RW<u32>, &reg.timer[1].ldvaln  as *const RW<u32>, "ldval1");
+            assert_eq!(0x4003_7114 as *const RO<u32>, &reg.timer[1].cvaln   as *const RO<u32>, "cval1");
+            assert_eq!(0x4003_7118 as *const RW<u32>, &reg.timer[1].tctrln  as *const RW<u32>, "tctrl1");
+            assert_eq!(0x4003_711C as *const RW<u32>, &reg.timer[1].tflgn   as *const RW<u32>, "tflg1");
         }
     }
 }
